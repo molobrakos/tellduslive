4872e8b56c2f56ef9097ef633580586040a9db3f
Erik <erik.eriksson@yahoo.com>
2016-12-11 11:53:31 +0100

    initial checkin

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="tellduslive",
+      version="0.1.1",
+      description="Communicate with Telldus Live",
+      url="https://github.com/molobrakos/tellduslive",
+      license="",
+      author="Erik",
+      author_email="Erik",
+      py_modules=["tellduslive"],
+      provides=["tellduslive"],)
