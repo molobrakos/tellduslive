371b13cd9279b9ec8c3b73d0eac20fe588cec34b
Erik Eriksson <molobrakos@users.noreply.github.com>
2016-12-12 13:07:17 +0100

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="tellduslive",
-      version="0.1.10",
+      version="0.1.11",
       description="Communicate with Telldus Live",
       url="https://github.com/molobrakos/tellduslive",
       license="",
