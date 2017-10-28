bd809fa15278babcd1336af3f4ed11b1b5915571
Erik Eriksson <molobrakos@users.noreply.github.com>
2017-10-28 12:58:42 +0200

    Bump version: 0.7.0 → 0.7.1

--- tellduslive.py
+++ tellduslive.py
@@ -10,7 +10,7 @@ from requests import Session
 from requests.compat import urljoin
 from requests_oauthlib import OAuth1Session
 
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 _LOGGER = logging.getLogger(__name__)
 
